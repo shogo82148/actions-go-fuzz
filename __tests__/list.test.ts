a7d55994a7106d131e6e1859d77b1d0f5fc36494
ICHINOSE Shogo <shogo82148@gmail.com>
2023-06-26 06:42:16 +0900

    fix: ESLint was configured to run ... However, that TSConfig does not

--- __tests__/list.test.ts
+++ __tests__/list.test.ts
@@ -13,7 +13,7 @@ test("wait 500 ms", async () => {
   const start = new Date();
   await wait(500);
   const end = new Date();
-  var delta = Math.abs(end.getTime() - start.getTime());
+  const delta = Math.abs(end.getTime() - start.getTime());
   expect(delta).toBeGreaterThan(450);
 });
 
@@ -25,5 +25,7 @@ test("test runs", () => {
   const options: cp.ExecFileSyncOptions = {
     env: process.env,
   };
-  console.log(cp.execFileSync(np, [ip], options).toString());
+
+  const result = cp.execFileSync(np, [ip], options).toString();
+  console.log(result);
 });
